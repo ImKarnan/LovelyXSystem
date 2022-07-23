320bc70f0de907c9b6515369ad04acfa3aa9f33a
ImKarnan <109649877+ImKarnan@users.noreply.github.com>
2022-07-23 12:51:49 +0530

    Update main_manager.py

--- Lovely_System/plugins/main_manager.py
+++ Lovely_System/plugins/main_manager.py
@@ -1,6 +1,6 @@
 import re
 
-from Lovely_System import lovely_logs, ENFORCERS, lovely, INSPECTORS
+from Lovely_System import Lovely_logs, ENFORCERS, INSPECTORS
 from Lovely_System.strings import (
     scan_request_string,
     reject_string,
