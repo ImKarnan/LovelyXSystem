b6f55e0a8c1068e718dd0db2ce0563251914a818
ImKarnan <109649877+ImKarnan@users.noreply.github.com>
2022-07-23 16:08:48 +0530

    Update extras.py

--- Lovely_System/plugins/extras.py
+++ Lovely_System/plugins/extras.py
@@ -56,7 +56,7 @@ async def addenf(event) -> None:
         with open(json_file, "w") as file:
             json.dump(data, file, indent=4)
         await System.send_message(event.chat_id, "Added to enforcers, Restarting...")
-        if not event.from_id.user_id in SIBYL:
+        if not event.from_id.user_id in Lovely:
             await add_enforcers(event.from_id.user_id, u_id)
         await System.disconnect()
         os.execl(sys.executable, sys.executable, *sys.argv)
